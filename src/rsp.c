b3aa64bba373825833bc3c82c7577d7aabb9e724
Jiri Daněk <jdanek@redhat.com>
2022-08-11 14:15:33 +0200

    multiple threads

--- src/rsp.c
+++ src/rsp.c
@@ -23,16 +23,22 @@ int main()
     char *backend_addr = argv[2];
     char *backend_port_str = argv[3];
 
+    // https://idea.popcount.org/2017-02-20-epoll-is-fundamentally-broken-12/
+    /*
+     * > Additionally, in edge triggered mode it's hard to avoid starvation:
+     */
+
     // https://stackoverflow.com/questions/21687695/getting-sigpipe-with-non-blocking-sockets-is-this-normal/
     signal(SIGPIPE, SIG_IGN);
 
     epoll_init();
 
-    create_server_socket_handler(server_port_str,
-                                 backend_addr,
-                                 backend_port_str);
+    create_server_socket_handler("5101", "127.0.0.1", "5201");
+    create_server_socket_handler("5102", "127.0.0.1", "5202");
+    create_server_socket_handler("5103", "127.0.0.1", "5203");
+    create_server_socket_handler("5104", "127.0.0.1", "5204");
 
-    rsp_log("Started.  Listening on port %s.", server_port_str);
+    rsp_log("Started.  Listening on port %s and maybe few other ones.", server_port_str);
     epoll_do_reactor_loop();
 
     return 0;
